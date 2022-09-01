f122c581da9d9b56eed5e92c0cf49cce919120c9
idhun90 <idhun90@naver.com>
2022-09-02 00:18:40 +0900

    줄 바꿈 여부에 따라 제목, 내용 구분 코드 추가

--- Memo Project/WriteEdit/WriteEditViewController.swift
+++ Memo Project/WriteEdit/WriteEditViewController.swift
@@ -38,11 +38,20 @@ class WriteEditViewController: BaseViewController {
         autoShowKeyboard()
         
     }
-    // 작성 화면 진입 시 키보드 자동 띄움
+    // 작성 화면 진입 시 키보드 띄움 및 공유, 완료 버튼 보이기
     func autoShowKeyboard() {
-        if self.mainView.textView.text.isEmpty {
+        if mainView.textView.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
             mainView.textView.becomeFirstResponder()
+            showRightBarButtonItems()
         }
+    }
+    
+    func showRightBarButtonItems() {
+        let shareButton = UIBarButtonItem(image: UIImage(systemName: "square.and.arrow.up"), style: .plain, target: self, action: #selector(shareButtonClicked))
+        
+        let finishButton = UIBarButtonItem(title: "완료", style: .done, target: self, action: #selector(finishButtonClicked))
+        
+        navigationItem.rightBarButtonItems = [finishButton,shareButton]
     }
     
     override func configureUI() {
@@ -61,16 +70,30 @@ class WriteEditViewController: BaseViewController {
     @objc func finishButtonClicked() {
         // 데이터 값 저장
         // 완료 버튼 누를 시 '완료'버튼 히든 처리, 및 키보드 내림
+
+        // 저장 전 줄바꿈 여부 체크
+        guard let originalText = mainView.textView.text else { return }
         
-        guard let title = mainView.textView.text else {
-            print("텍스트뷰에 작성된 내용이 없습니다.")
-            return
+        if originalText.contains("\n") && !originalText.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+            
+            // 제목 앞에 여러 줄바꿈이 있다면 해당 줄바꿈 제거 후 첫 번째 요소 타이틀로 선정
+            // 애플 메모앱은 테이블뷰 제목 항목에 공백 없는 타이틀이 보이지만, 화면 내용이나 수정 화면으로 보이면 공백도 함께 보여진다. 어떻게 처리한걸까
+            let title = originalText.trimmingCharacters(in: .whitespacesAndNewlines).components(separatedBy: "\n")[0]
+            // 공백을 제거한 상태에서 타이틀을 제외한 substring 타입의 나머지 요소
+            let contentSubstring = originalText.trimmingCharacters(in: .whitespacesAndNewlines).dropFirst(title.count)
+            let content = String(contentSubstring)
+            let memo = RealmMemo(realmTitle: title, realmContent: content, realmCreatedDate: Date(), realmEditedDate: nil)
+            repository.fetchRealmAddItem(item: memo)
+            
+        } else if !originalText.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+            
+            let title = originalText
+            let memo = RealmMemo(realmTitle: title, realmContent: nil, realmCreatedDate: Date(), realmEditedDate: nil)
+            repository.fetchRealmAddItem(item: memo)
+            
+        } else {
+            print("저장할 텍스트 내용이 없습니다.")
         }
-        // 엔터키를 치지 않았다면
-        let memo = RealmMemo(realmTitle: title, realmContent: nil, realmCreatedDate: Date(), realmEditedDate: nil)
-        repository.fetchRealmAddItem(item: memo)
-        
-        
     }
 }
 
@@ -78,19 +101,62 @@ extension WriteEditViewController: UITextViewDelegate {
     
     func textViewDidBeginEditing(_ textView: UITextView) {
         print(#function)
+        showRightBarButtonItems()
     }
     
+    // 공유, 완료 버튼
     func textViewDidChange(_ textView: UITextView) {
         print(#function)
+        print("공백 체크: ", textView.text.isEmpty)
+        print("=================================")
+        print("스페이스, 줄바꿈 조건 제거 공백 체크:", textView.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty) //스페이스, 공백 제거
+        print("=================================")
+        print("줄바꿈 체크:", textView.text.contains("\n"))
+        print("=================================")
+        let titleText = textView.text.components(separatedBy: "\n")[0]
+        print("제목: ", titleText)
+        let subTextByDrop = textView.text.dropFirst(titleText.count)
+        print("내용:", subTextByDrop)
         
-        let shareButton = UIBarButtonItem(image: UIImage(systemName: "square.and.arrow.up"), style: .plain, target: self, action: #selector(shareButtonClicked))
+        print("첫 줄 공백", textView.text.components(separatedBy: "\n")[0].isEmpty) // -> 내용이 제목이 되야함.
+        print("공백, 스페이스 제거:", textView.text.trimmingCharacters(in: .whitespacesAndNewlines).components(separatedBy: "\n")[0])
         
-        let finishButton = UIBarButtonItem(title: "완료", style: .done, target: self, action: #selector(finishButtonClicked))
+        let resulttitle = textView.text.trimmingCharacters(in: .whitespacesAndNewlines).components(separatedBy: "\n")[0]
+        print("공백, 스페이스 제거 내용:", textView.text.trimmingCharacters(in: .whitespacesAndNewlines).dropFirst(resulttitle.count))
         
-        if textView.text.isEmpty {
-            navigationItem.rightBarButtonItems = nil
-        } else {
-            navigationItem.rightBarButtonItems = [finishButton,shareButton]
+        // 테이블쎌에 보여줄 제목은 공백이 제거된 제목 -> resultTitle
+        // 하지만 메모장에 보여줄 때는 공백까지 함께 보여줘야 함. 공백이 포함된 제목
+        
+        
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        print(#function)
+        // 백버튼 또는 제스처로 이전 화면 복귀 시 해당 메소드 호출 확인
+        // 이 곳에서 데이터 저장 또는 값 전달이 이뤄져야 한다. (공백이 아닐 경우에만)
+        
+        if !textView.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+            // 값 전달을 해야 합니다.
+            if textView.text.contains("\n") {
+                
+            } else {
+                
+            }
         }
     }
 }
+
+// 문자열 테스트
+// 줄바꿈 기준 문자열 분리
+//print(textView.text.split(separator: "\n"))
+//print(textView.text.components(separatedBy: "\n")) // 여러 엔터 값을 가지면 split 보다는 comonents로 " " 표기해주는 것이 좋지 않을까
+
+// 분리된 문자열 타이틀, 내용 분리
+//let titleText = textView.text.components(separatedBy: "\n")[0] \n 기준 분리 시 첫 번째 요소
+//print("제목:", titleText)
+//let subtitleText = textView.text.components(separatedBy: "\n").last \n 기준 분리 시 마지막 요소, 내용 중에 줄 바꿈이 발생하면 적합하지 않음
+//print("제목을 뺀 나머지:", subtitleText)
+//let subTextByDrop = textView.text.dropFirst(titleText.count) 분리된 첫 번째 요소를 제거하고 나머지 요소 보여줌
+//print("내용:", subTextByDrop)
+//let subTextByremove = textView.text.removeFirst() // 텍스트뷰에서 하나 하나 입력할 때마다 확인을 하려고 했는데 원본을 계속 수정에서 일단 패스
+//print(subTextByremove)
